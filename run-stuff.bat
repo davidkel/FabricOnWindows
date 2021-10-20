002e6a4415f25c4c3c18cac89e2c10555437dd59
D <d_kelsey@uk.ibm.com>
2021-10-20 10:23:56 +0100

    fix run-stuff
    Signed-off-by: D <d_kelsey@uk.ibm.com>

--- run-stuff.bat
+++ run-stuff.bat
@@ -9,12 +9,12 @@ set CORE_PEER_ADDRESS=127.0.0.1:7051
 set CORE_PEER_LOCALMSPID=Org1MSP
 set CORE_PEER_MSPCONFIGPATH=..\..\crypto-config\peerOrganizations\org1.example.com\users\Admin@org1.example.com\msp
 
-rem peer chaincode invoke -o 127.0.0.1:6050 -C mychannel -n basic -c '{"Args":["createChaosAsset","c1","99"]}' 
-rem --tls --cafile "../../crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/tls/ca.crt"
-set FNARGS={\"Args\":[\"readAsset\",\"c1\"]}
-set
+peer chaincode invoke
+
+peer chaincode invoke -o 127.0.0.1:6050 -C mychannel -n basic -c {\"Args\":[\"createChaosAsset\",\"c1\",\"99\"]} --tls --cafile "../../crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/tls/ca.crt" --waitForEvent
+set FNARGS={\"Args\":[\"readChaosAsset\",\"c1\"]}
 peer chaincode query -C mychannel -n basic -c %FNARGS%
 
-rem peer chaincode invoke -o 127.0.0.1:6050 -C mychannel -n basic -c '{"Args":["updateChaosAsset","c1","65"]}' --tls --cafile "../../crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/tls/ca.crt"
+peer chaincode invoke -o 127.0.0.1:6050 -C mychannel -n basic -c {\"Args\":[\"updateChaosAsset\",\"c1\",\"65\"]} --tls --cafile "../../crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/tls/ca.crt" --waitForEvent
 
-rem peer chaincode query -C mychannel -n basic -c '{"Args":["readAsset","c1"]}'
+peer chaincode query -C mychannel -n basic -c %FNARGS%
