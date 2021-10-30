3a4a4e4f22f23b063981e692a5068a80e63209e5
D <d_kelsey@uk.ibm.com>
2021-10-30 14:45:38 +0100

    Microfab as launcher
    Signed-off-by: D <d_kelsey@uk.ibm.com>

---
+++ runmicrofab.bat
@@ -0,0 +1,3 @@
+set FABRIC_CFG_PATH=C:\_SourceCode\github-mine\FabricOnWindows\test-env\config
+set PATH=C:\_SourceCode\github-mine\FabricOnWindows\test-env\bin;%PATH%
+set CORE_LEDGER_SNAPSHOTS_ROOTDIR=/var/hyperledger
